77a238bd438b725f9ef06027c95a9e1b5d52b296
Derek Townsend <derekjavon2@gmail.com>
2019-06-06 10:36:00 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,42 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+# Form page to create Article
+  get '/articles/new' do
+    erb :new
+  end
 
-  get '/' do
+#Create and redirect
+  post '/articles' do
+    @new_article = Article.create(params)
+    redirect :"/articles/#{@new_article.id}"
+  end
+
+# Show all
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+# Show Specific
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+# Form to update Article
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end
+
+#Create and redirect
+    patch '/articles/:id' do
+      @updated_article = Article.update(params[:id], title: params[:title], content: params[:content])
+      redirect :"/articles/#{@updated_article.id}"
+    end
+#Deletion
+  delete '/articles/:id' do
+    Article.find_by(id: params[:id]).destroy
+    redirect :'/articles'
   end
 end
